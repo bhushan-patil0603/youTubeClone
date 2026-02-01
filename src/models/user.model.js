f58dc9fb57a0f416f5d8150d003cd722b543ad93
Bhushan <pbhushan0603@gmail.com>
2026-02-02 00:53:20 +0530

    added database models

---
+++ src/models/user.model.js
@@ -0,0 +1,49 @@
+import mongoose, { Schema } from "mongoose";
+const userSchema = new Schema(
+  {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowerCase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowerCase: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timeStamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
